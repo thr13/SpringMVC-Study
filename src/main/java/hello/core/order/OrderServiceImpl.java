37d3894859184d020a3178312528da59ca314fa4
sugwan <skhrnt2945@gmail.com>
2025-06-26 20:15:09 +0900

    feat: 주문 서비스

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,21 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice); // 할인은 오직 discountPolicy 만 담당하므로 SRP 을 만족함
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
