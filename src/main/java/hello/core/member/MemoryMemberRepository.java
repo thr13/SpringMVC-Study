28ccdada50e3728e07bb48faab90c15de91190a0
sugwan <skhrnt2945@gmail.com>
2025-06-26 20:13:55 +0900

    feat: 회원 리포지토리

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>(); // 실무에서는 동시성 이슈를 방지하기 위해 HashMap 보단 ConcurrentHashMap 사용을 추천함
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
