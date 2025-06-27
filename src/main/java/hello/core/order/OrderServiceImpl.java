0a15b85700d9487d976a9f3f0f82dfdebb3365e8
sugwan <skhrnt2945@gmail.com>
2025-06-27 12:02:22 +0900

    issue: OrderServiceImpl 문제점  - 현재 구현체에 의존하고 있는 문제 발생  - 클라이언트 OrderServiceImpl 이 구현체 FixDiscountPolicy, RateDiscountPolicy 에 의존하고 있기 때문에 할인정책 변경시 OrderServiceImpl 이 수정되어야 함 그러므로 DIP, OCP 위반

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,20 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
+    /*
+    할인 정책의 클라이언트 OrderServiceImpl
+
+    추상체뿐만 아니라 구현체에 의존하고 있기 때문에 '할인정책' 변경시 여기 코드도 수정해야한다 -> OCP, DIP 위반
+    추상(인터페이스) 의존: DiscountPolicy
+    구체(구현 클래스): FixDiscountPolicy, RateDiscountPolicy
     private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    해결방법:
+    제3자가 클라이언트인 OrderServiceImpl 에 추상체인 DiscountPolicy 의 구현 객체를 대신 생성하고 주입해야함
+     */
+    private DiscountPolicy discountPolicy;
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
