8791969fe3254c93a0f2d3f662973c6a25af1c68
sugwan <skhrnt2945@gmail.com>
2025-06-28 20:37:07 +0900

    fix: AppConfig 를 스프링이 관리하도록 수정

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,26 +9,34 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /*
 애플리케이션 전체를 설정하고 구성하는 클래스(환경설정 담당)
 각 서비스 계층에서 구현체를 직접 할당하는게 아닌, AppConfig 가 대신 구현체를 생성해주고 이를 서비스계층에서 주입시켜, 의존관계를 변경함
 즉, 서비스 계층 구현체의 생성자 주입을 통해서, 리파지토리 구현체를 생성함
  */
+
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(new MemoryMemberRepository());
     }
 
-    private MemberRepository memberRepository() { // DB 를 변경할 경우 이 메소드만 수정하면 된다
+    @Bean
+    public MemberRepository memberRepository() { // DB 를 변경할 경우 이 메소드만 수정하면 된다
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(discountPolicy(), memberRepository());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() { // 할인 정책이 변경될 경우 이 메소드만 수정하면 된다
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
