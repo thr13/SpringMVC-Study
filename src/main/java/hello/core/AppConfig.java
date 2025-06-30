e0367d671d7e6c5209047225380f7e464f956ada
sugwan <skhrnt2945@gmail.com>
2025-06-30 17:21:24 +0900

    test: 스프링 컨테이너와 싱글톤

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -16,6 +16,8 @@ import org.springframework.context.annotation.Configuration;
 애플리케이션 전체를 설정하고 구성하는 클래스(환경설정 담당)
 각 서비스 계층에서 구현체를 직접 할당하는게 아닌, AppConfig 가 대신 구현체를 생성해주고 이를 서비스계층에서 주입시켜, 의존관계를 변경함
 즉, 서비스 계층 구현체의 생성자 주입을 통해서, 리파지토리 구현체를 생성함
+
+추가: 스프링 컨테이너는 기본적으로 객체를 싱글톤으로 만들어서 관리해 줌
  */
 
 @Configuration
