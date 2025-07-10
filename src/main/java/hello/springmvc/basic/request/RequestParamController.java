a9fe7aa8f7164b0558b1dd2d15737f715288133b
sugwan <skhrnt2945@gmail.com>
2025-07-10 18:04:59 +0900

    feat: HTTP 요청 파라미터 - HttpServletRequest, HttpServletResponse 을 이용

---
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,24 @@
+package hello.springmvc.basic.request;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+
+    // HttpServletRequest 가 제공하는 GET 파라미터
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+        log.info("username={}, age={}", username, age);
+
+        response.getWriter().write("ok"); // 반환타입이 void 여도 response 에 값을 쓰면 메시지 바디에 그대로 들어간다
+    }
+}
