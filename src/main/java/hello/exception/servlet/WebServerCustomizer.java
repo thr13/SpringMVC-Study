0d1cb6d012b4a2558aeacf9b06fd891bcd238340
sugwan <skhrnt2945@gmail.com>
2025-07-23 10:30:46 +0900

    feat: WebServerFactoryCustomizer 를 이용한 에러 페이지 변경

---
+++ src/main/java/hello/exception/servlet/WebServerCustomizer.java
@@ -0,0 +1,21 @@
+package hello.exception.servlet;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        // Spring Boot 가 제공하는 에러 페이지
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500"); // RuntimeException 예외(또는 RuntimeException 자식 타입의 예외) 발생시 호출할 예외
+
+        factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
+    }
+}
