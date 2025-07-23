3423aad619f5094065a7c346591868721ddbad2b
thr13 <skhrnt2945@gmail.com>
2025-07-23 18:03:28 +0900

    feat: servlet - API 예외 처리(json)

--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -1,4 +1,4 @@
-package hello.exception.servlet;
+package hello.exception;
 
 import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.ErrorPage;
@@ -6,15 +6,14 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
-        // Spring Boot 가 제공하는 에러 페이지
         ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
         ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
-        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500"); // RuntimeException 예외(또는 RuntimeException 자식 타입의 예외) 발생시 호출할 예외
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
 
         factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
     }
