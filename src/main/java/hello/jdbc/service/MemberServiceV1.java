120320559b1840eccb330dafcc73945d076ff219
sugwan <skhrnt2945@gmail.com>
2025-07-28 17:05:38 +0900

    feat: 트랜잭션 적용

---
+++ src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -0,0 +1,29 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.SQLException;
+
+@RequiredArgsConstructor
+public class MemberServiceV1 {
+
+    private final MemberRepositoryV1 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId); //계좌에서 돈을 꺼냄
+        Member toMember = memberRepository.findById(toId); //꺼낸 돈을 계좌에 넣음
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    // 입금 처리과정에서 예외(ex)가 발생할 경우
+    private static void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
