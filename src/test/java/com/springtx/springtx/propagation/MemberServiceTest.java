b6feed75abf94cb4cf608d7df00e094c02ac3094
thr13 <skhrnt2945@gmail.com>
2025-08-05 14:12:34 +0900

    test: 트랜잭션 예외시 트랜잭션 처리 확인

--- src/test/java/com/springtx/springtx/propagation/MemberServiceTest.java
+++ src/test/java/com/springtx/springtx/propagation/MemberServiceTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 @Slf4j
 @SpringBootTest
 class MemberServiceTest {
@@ -31,7 +34,27 @@ class MemberServiceTest {
         //when
         memberService.joinV1(username);
         //then
-        Assertions.assertTrue(memberRepository.find(username).isPresent());
-        Assertions.assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * memberService    @Transactional:OFF
+     * memberRepository @Transactional:ON
+     * logRepository    @Transactional:ON Exception
+     */
+    @DisplayName("Log 예외가 발생하는 경우")
+    @Test
+    void outerTxOff_fail() {
+        //given
+        String username = "로그예외_outerTxOff_fail";
+
+        //when
+        assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+
+        //then
+        assertTrue(memberRepository.find(username).isPresent()); // Member 의 트랜잭션은 커밋됨
+        assertTrue(logRepository.find(username).isEmpty()); // Log 의 트랜잭션은 롤백되므로 비어있다
     }
 }
