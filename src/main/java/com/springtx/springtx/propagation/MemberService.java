57743b525d70092f3e9dbc05584261286264da01
thr13 <skhrnt2945@gmail.com>
2025-08-05 12:50:36 +0900

    feat: Member 도메인

---
+++ src/main/java/com/springtx/springtx/propagation/MemberService.java
@@ -0,0 +1,52 @@
+package com.springtx.springtx.propagation;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final LogRepository logRepository;
+
+
+    public void joinV1(String username) {
+        Member member = new Member(username);
+        Log logMessage = new Log(username);
+
+        // 각각 트랜잭션 사용
+        log.info("== memberRepository 호출 시작 ==");
+        memberRepository.save(member);
+        log.info("== memberRepository 호출 종료 ==");
+
+        log.info("== logRepository 호출 시작 ==");
+        logRepository.save(logMessage);
+        log.info("== logRepository 호출 종료 ==");
+    }
+
+    /**
+     * DB 로그를 저장할때 예외가 발생할 경우, 예외를 복구한다
+     * 현재 별도의 트랜잭션을 설정하지 않음
+     * @param username
+     */
+    public void joinV2(String username) {
+        Member member = new Member(username);
+        Log logMessage = new Log(username);
+
+        log.info("== memberRepository 호출 시작 ==");
+        memberRepository.save(member);
+        log.info("== memberRepository 호출 종료 ==");
+
+        log.info("== logRepository 호출 시작 ==");
+        try {
+            logRepository.save(logMessage);
+        } catch (RuntimeException e) {
+            log.info("log 저장에 실패헀습니다. logMessage={}", logMessage.getMessage());
+            log.info("정상 흐름 반환");
+        }
+        log.info("== logRepository 호출 종료 ==");
+    }
+}
