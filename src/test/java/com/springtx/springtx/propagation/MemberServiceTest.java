dc912b325b4da93df5759b376bdbacbdfbe395ae
thr13 <skhrnt2945@gmail.com>
2025-08-05 12:53:23 +0900

    test: member 트랜잭션 실행 확인

---
+++ src/test/java/com/springtx/springtx/propagation/MemberServiceTest.java
@@ -0,0 +1,37 @@
+package com.springtx.springtx.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    LogRepository logRepository;
+
+    /**
+     * memberService    @Transactional:OFF
+     * memberRepository @Transactional:ON
+     * logRepository    @Transactional:ON
+     */
+    @DisplayName("외부 트랜잭션이 없는 경우")
+    @Test
+    void outerTxOff_success() {
+        //given
+        String username = "outerTxOff_success";
+        //when
+        memberService.joinV1(username);
+        //then
+        Assertions.assertTrue(memberRepository.find(username).isPresent());
+        Assertions.assertTrue(memberRepository.find(username).isPresent());
+    }
+}
