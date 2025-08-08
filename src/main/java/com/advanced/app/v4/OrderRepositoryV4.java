15cdcc848e3e6bc4b11cc5886529cbba15ac210e
sugwan <skhrnt2945@gmail.com>
2025-08-08 22:33:07 +0900

    feat: 로그 추적기에 템플릿 메서드 패턴 적용

---
+++ src/main/java/com/advanced/app/v4/OrderRepositoryV4.java
@@ -0,0 +1,40 @@
+package com.advanced.app.v4;
+
+import com.advanced.trace.TraceStatus;
+import com.advanced.trace.logtrace.LogTrace;
+import com.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV4 {
+
+    private final LogTrace trace;
+
+    public void save(String itemId) {
+
+        AbstractTemplate<Void> template = new AbstractTemplate<>(trace) {
+
+            @Override
+            protected Void call() {
+                if (itemId.equals("ex")) {
+                    throw new IllegalStateException("예외 발생!");
+                }
+                sleep(1000);
+
+                return null;
+            }
+        };
+
+        template.execute("OrderRepository.save()");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
