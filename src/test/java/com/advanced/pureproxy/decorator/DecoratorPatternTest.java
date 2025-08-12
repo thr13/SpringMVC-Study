4f5391f5601602decd3d7cb2dda9309135bb7b34
thr13 <skhrnt2945@gmail.com>
2025-08-12 18:45:43 +0900

    test: 데코레이터 패턴
    데코레이터 패턴은 프록시 이므로 체인이 가능하다 그리고 클라이언트 코드 변경없이 주입대상만 변경하여 사용할 수 있다

--- src/test/java/com/advanced/pureproxy/decorator/DecoratorPatternTest.java
+++ src/test/java/com/advanced/pureproxy/decorator/DecoratorPatternTest.java
@@ -1,8 +1,6 @@
 package com.advanced.pureproxy.decorator;
 
-import com.advanced.pureproxy.decorator.code.Component;
-import com.advanced.pureproxy.decorator.code.DecoratorPatternClient;
-import com.advanced.pureproxy.decorator.code.RealComponent;
+import com.advanced.pureproxy.decorator.code.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -15,4 +13,22 @@ public class DecoratorPatternTest {
         DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
         client.execute();
     }
+
+    @Test
+    void decorator1() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator); //데코레이터 패턴은 클라이언트 코드수정없이 주입한 부분만 변경하면 된다
+        client.execute();
+    }
+
+    @Test
+    void decorator2() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        Component timeDecorator = new TimeDecorator(messageDecorator); //데코페이터 패턴은 프록시 이므로 체인이 가능하다
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
+        client.execute();
+    }
+
 }
