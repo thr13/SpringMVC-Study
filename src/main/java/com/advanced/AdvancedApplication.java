9805307dc3807b6378d6aecadee48410250c9676
sugwan <skhrnt2945@gmail.com>
2025-08-13 21:17:56 +0900

    feat: 동적 프록시 적용

--- src/main/java/com/advanced/AdvancedApplication.java
+++ src/main/java/com/advanced/AdvancedApplication.java
@@ -4,6 +4,7 @@ import com.advanced.config.AppV1Config;
 import com.advanced.config.AppV2Config;
 import com.advanced.config.v1_proxy.ConcreteProxyConfig;
 import com.advanced.config.v1_proxy.InterfaceProxyConfig;
+import com.advanced.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import com.advanced.trace.logtrace.LogTrace;
 import com.advanced.trace.logtrace.ThreadLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -13,7 +14,8 @@ import org.springframework.context.annotation.Import;
 
 //@Import(AppV1Config.class)
 //@Import({InterfaceProxyConfig.class, AppV2Config.class})
-@Import(ConcreteProxyConfig.class)
+//@Import(ConcreteProxyConfig.class)
+@Import({DynamicProxyBasicConfig.class, AppV2Config.class})
 @SpringBootApplication(scanBasePackages = "com.advanced.app")
 public class AdvancedApplication {
 
@@ -21,10 +23,8 @@ public class AdvancedApplication {
 		SpringApplication.run(AdvancedApplication.class, args);
 	}
 
-	/*
 	@Bean
 	public LogTrace logTrace() {
 		return new ThreadLogTrace();
 	}
-	 */
 }
---
+++ src/main/java/com/advanced/config/v2_dynamicproxy/DynamicProxyBasicConfig.java
@@ -0,0 +1,44 @@
+package com.advanced.config.v2_dynamicproxy;
+
+import com.advanced.app.log.v1.OrderRepositoryV1;
+import com.advanced.app.proxy.v1.*;
+import com.advanced.config.v2_dynamicproxy.handler.LogTraceBasicHandler;
+import com.advanced.trace.logtrace.LogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.reflect.Proxy;
+
+@Configuration
+public class DynamicProxyBasicConfig {
+
+    @Bean
+    public ProxyOrderControllerV1 dynamicProxyOrderControllerV1(LogTrace logTrace) {
+        ProxyOrderControllerV1 orderController = new ProxyOrderControllerV1Impl(dynamicProxyOrderServiceV1(logTrace));
+
+        ProxyOrderControllerV1 proxy = (ProxyOrderControllerV1) Proxy.newProxyInstance(ProxyOrderControllerV1.class.getClassLoader(),
+                new Class[]{ProxyOrderControllerV1.class},
+                new LogTraceBasicHandler(orderController, logTrace));
+        return proxy;
+    }
+
+    @Bean
+    public ProxyOrderServiceV1 dynamicProxyOrderServiceV1(LogTrace logTrace) {
+        ProxyOrderServiceV1 orderService = new ProxyOrderServiceV1Impl(dynamicProxyOrderRepositoryV1(logTrace));
+
+        ProxyOrderServiceV1 proxy = (ProxyOrderServiceV1) Proxy.newProxyInstance(ProxyOrderServiceV1.class.getClassLoader(),
+                new Class[]{ProxyOrderServiceV1.class},
+                new LogTraceBasicHandler(orderService, logTrace));
+        return proxy;
+    }
+
+    @Bean
+    public ProxyOrderRepositoryV1 dynamicProxyOrderRepositoryV1(LogTrace logTrace) {
+        ProxyOrderRepositoryV1 orderRepository = new ProxyOrderRepositoryV1Impl();
+
+        ProxyOrderRepositoryV1 proxy = (ProxyOrderRepositoryV1) Proxy.newProxyInstance(ProxyOrderRepositoryV1.class.getClassLoader(),
+                new Class[]{ProxyOrderRepositoryV1.class},
+                new LogTraceBasicHandler(orderRepository, logTrace));
+        return proxy;
+    }
+}
