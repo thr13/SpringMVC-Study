457aa55f6a83336ccfd974d621f8554931f7037d
sugwan <skhrnt2945@gmail.com>
2025-08-15 10:19:32 +0900

    feat: 메소드 시그니쳐 출력 로그

--- src/test/java/com/advanced/aop/AopTest.java
+++ src/test/java/com/advanced/aop/AopTest.java
@@ -2,15 +2,18 @@ package com.advanced.aop;
 
 import com.advanced.order.OrderRepository;
 import com.advanced.order.OrderService;
+import com.advanced.order.aop.AspectV1;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
+@Import(AspectV1.class)
 public class AopTest {
 
     @Autowired
